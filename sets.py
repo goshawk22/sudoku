053119bce31e6d672d2457fbae4392d169cf2ef2
Adam Lawson <adamjlawson22@gmail.com>
2021-02-07 17:19:53 +0000

    Initial Harder Logic

--- sets.py
+++ sets.py
@@ -2,3 +2,10 @@ a = {1,4,6,8}
 b = {2,5,6,8,6}
 
 print(a-b)
+
+a = [1, 2, 3, 4, 5, 6, 6, 7, 9]
+print(a.index(6))
+
+import numpy as np
+a = np.array(a).reshape(3,3)
+print(np.argwhere(a==6))
--- sudoku.py
+++ sudoku.py
@@ -2,8 +2,9 @@ import numpy as np
 import time
 start_time = time.time()
 subtractSet = {1,2,3,4,5,6,7,8,9}
+reference_grid = np.array(range(81)).reshape(9,9)
 
-puzzle = np.array(
+puzzle1 = np.array(
     [4,0,1,8,0,0,0,5,3,
     8,0,5,0,7,4,0,0,6,
     0,0,7,5,3,0,0,1,8,
@@ -14,31 +15,54 @@ puzzle = np.array(
     3,0,0,0,8,1,5,0,7,
     5,8,0,3,4,0,9,0,0]
 )
-puzzle = puzzle.reshape(9,9)
-print(puzzle)
+
+puzzle1 = np.array(
+    [2,0,4,0,0,0,1,0,0,
+    1,0,7,0,9,0,8,0,2,
+    0,0,0,0,0,0,7,0,0,
+    6,0,0,0,3,1,9,0,4,
+    0,4,0,0,2,0,6,1,3,
+    8,3,1,6,4,9,2,5,7,
+    4,0,0,9,6,0,3,7,0,
+    0,0,0,0,0,0,4,2,9,
+    0,0,0,3,0,0,5,6,0]
+)
+
+puzzle1 = np.array(
+    [0,5,0,6,0,0,0,2,0,
+    0,0,3,8,0,0,7,0,6,
+    7,0,0,0,4,0,0,0,0,
+    0,0,0,0,7,4,9,0,0,
+    0,0,0,1,0,8,0,0,0,
+    0,0,1,3,5,0,0,0,0,
+    0,0,0,0,9,0,0,0,8,
+    2,0,4,0,0,1,6,8,8,
+    0,8,0,0,0,6,0,3,0]
+)
+
+puzzle1 = puzzle1.reshape(9,9)
 
 
+# Returns all the current values in the horizontal y.
 def checkHorizontal(y, puzzle):
-    #Returns all the current values in the horizontal y.
     horizontal = []
     for x in range(9):
         if puzzle[y,x] != 0:
             horizontal.append(puzzle[y,x])
+
     return set(horizontal)
 
-
+# Returns all the current values in the vertical x.
 def checkVertical(x, puzzle):
-    #Returns all the current values in the vertical x.
     vertical = []
     for y in range(9):
         if puzzle[y,x] != 0:
             vertical.append(puzzle[y,x])
     return set(vertical)
 
-
+#Checks possible values in a 3x3 split
 def checkSquare(square, puzzle):
-    #Checks possible values in a 3x3 split
-    splits = [] #COntains a list of all the squares (len 9)
+    splits = [] #Contains a list of all the squares (len 9)
     numbers = [] #Contains all the numbers in the square square
     horizontalSplit = np.hsplit(puzzle, 3)
     for split in horizontalSplit:
@@ -55,19 +79,16 @@ def checkSquare(square, puzzle):
 
     return numbers
 
-
+#Gets the possible numbers for a square
 def getPossible(square, puzzle):
-    #Gets the possible numbers for a square
     vertical = subtractSet - checkVertical(square[1], puzzle)
     horizontal = subtractSet - checkHorizontal(square[0], puzzle)
-    square = subtractSet - checkSquare(getThreeOfSquare(square, puzzle), puzzle)
-    #print(vertical, horizontal, square)
+    square = subtractSet - checkSquare(getThreeOfSquare(square), puzzle)
     valids = vertical.intersection(horizontal).intersection(square)
     return list(valids)
 
-
-def getThreeOfSquare(square, puzzle):
-    #Gets which 3x3 square a number/square is in.
+# Gets which 3x3 square a number/square is in.
+def getThreeOfSquare(square):
     if square[1] < 3:
         if square[0] < 3:
             threeSquare = 0
@@ -93,37 +114,153 @@ def getThreeOfSquare(square, puzzle):
     return threeSquare
 
 
+### Harder Logic ###
+
+# Returns a list of coordinates that are empty
+def getEmptySquares(puzzle):
+    return list(np.argwhere(puzzle == 0))
+
+# Returns a list of length 9 with the valid numbers for each square in the row
+def getValidsForRow(row_num, puzzle):
+    valids = []
+    for i in range(9):
+        if puzzle[row_num, i] == 0:
+            valids.append(getPossible([row_num, i], puzzle))
+        else:
+            valids.append([])
+    
+    return valids
+
+# Returns a list of length 9 with the valid numbers for each square in the column
+def getValidsForColumn(column_num, puzzle):
+    valids = []
+    for i in range(9):
+        if puzzle[i, column_num] == 0:
+            valids.append(getPossible([i, column_num], puzzle))
+        else:
+            valids.append([])
+    
+    return valids
+
+# Gets a list of coordinates of squares that make up a 3x3 square
+def getSquaresInBigSquares(big_square):
+    coords = []
+    for x in range(9):
+        for y in range(9):
+            if getThreeOfSquare([x,y]) == big_square:
+                coords.append([x,y])
+    
+    assert len(coords) == 9
+    return coords
+
+# Returns a list of length 9 with the valid numbers for each square in the 3x3 square and a list of coordinates that each valids corresponds to
+def getValidsForSquare(square_num, puzzle):
+    valids = []
+    squares = getSquaresInBigSquares(square_num)
+    for i in squares:
+        if puzzle[tuple(i)] == 0:
+            valids.append(getPossible(i, puzzle))
+        else:
+            valids.append([])
+    
+    return valids, squares
+
+#Gets the possible numbers for a square
+def getPossibleWithList(square, puzzle):
+    vertical = subtractSet - checkVertical(square[1], puzzle)
+    horizontal = subtractSet - checkHorizontal(square[0], puzzle)
+    square = subtractSet - checkSquare(getThreeOfSquare(square), puzzle)
+    valids = vertical.intersection(horizontal).intersection(square)
+    return list(valids)
+
+def hardSolver(puzzle):
+    tempPuzzle = puzzle
+    for num in range(9):
+
+        # Naked Pairs
+        # Rows
+        valids = getValidsForRow(num, puzzle)
+        for valid in valids:
+            if (valids.count(valid) > 1) and (valid != []):
+                duplicate_valids = []
+                for v in range(len(valids)):
+                    if valids[v] == valid:
+                        duplicate_valids.append(v)
+
+                for i in duplicate_valids:
+                    print(valid)
+                    tempPuzzle[num,i] = valid
+
+        # Columns
+        valids = getValidsForColumn(num, puzzle)
+        for valid in valids:
+            if (valids.count(valid) > 1) and (valid != []):
+                duplicate_valids = []
+                for v in range(len(valids)):
+                    if valids[v] == valid:
+                        duplicate_valids.append(v)
+
+                for i in duplicate_valids:
+                    print(valid)
+                    tempPuzzle[i,num] = valid
+        
+        # Squares
+        valids, coords = getValidsForSquare(num, puzzle)
+        for valid in valids:
+            if (valids.count(valid) > 1) and (valid != []):
+                duplicate_valids = []
+                for v in range(len(valids)):
+                    if valids[v] == valid:
+                        duplicate_valids.append(v)
+                for i in duplicate_valids:
+                    print(valid)
+                    tempPuzzle[tuple(coords[i])] = valid
+
+    return tempPuzzle
+
 def getSolved(puzzle):
     if 0 in puzzle:
         return False
     else:
         return True
 
-#iter = 0
-#error = 0
-while True:
-    #iter += 1
-    #print(iter)
-    #if iter % 1000:
-        #print(puzzle)
-    #oldPuzzle = puzzle
-    for x in range(9):
-        for y in range(9):
-            if puzzle[x,y] == 0:
-                valid = getPossible([x,y], puzzle)
-                if len(valid) == 1:
-                    puzzle[x,y] = valid[0]
-                    #print(valid)
-    #if np.array_equiv(oldPuzzle, puzzle):
-        #print("No progress was made.")
-        #error += 1
-        #if error > 10:
-            #print("Error threshhold reached. Aborting...")
-            #break
-    if not 0 in puzzle:
-        break
+def solve(puzzle):
+    depth = 0
+    while not getSolved(puzzle):
+        for coordinate in getEmptySquares(puzzle):
+            oldPuzzle = puzzle.copy()
+            valid = getPossible(coordinate, puzzle)
+            if len(valid) == 1:
 
-print(puzzle)
-#print(0 in puzzle)
+                puzzle[coordinate[0], coordinate[1]] = valid[0]
+
+                assert not np.array_equiv(puzzle, oldPuzzle)
+                
+                '''
+                tempPuzzle = hardSolver(puzzle)
+                results = solveDepthOne(tempPuzzle)
+                if results == None:
+                    continue
+                else:
+                    puzzle[results[0], results[1]] = results[3]
+                '''
+
+        if np.array_equiv(oldPuzzle, puzzle):
+            depth += 1
+        
+        if depth == 10:
+            print(puzzle)
+            raise RecursionError
+
+def solveDepthOne(puzzle):
+    for coordinate in getEmptySquares(puzzle):
+        valid = getPossible(coordinate, puzzle)
+        if len(valid) == 1:
+            puzzle[coordinate[0], coordinate[1]] = valid[0]
+            return (coordinate[0], coordinate[1], valid[0])
+    
+
+
+solve(puzzle1.copy())
 
 print("--- %s seconds ---" % (time.time() - start_time))
